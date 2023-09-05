9cdabd76e479328752b6c041d66e99fa7cdc8abd
Sundus <116579651+Doudou0287@users.noreply.github.com>
2023-09-05 15:06:25 +0200

    fix user logout

--- routes/routes.js
+++ routes/routes.js
@@ -16,4 +16,17 @@ router.post('/signUp' , signUpOrLogin);
 
 router.get('/winners', winners);
 
+router.get('/logout', (req, res) => {
+    // Destroy the user's session to reset everything
+    req.session.destroy(err => {
+      if (err) {
+        console.error('Error destroying session:', err);
+        // Handle the error if necessary
+      } else {
+        // Redirect the user to a page after logout (e.g., home page)
+        res.redirect('/');
+      }
+    });
+});
+
 export default router;
